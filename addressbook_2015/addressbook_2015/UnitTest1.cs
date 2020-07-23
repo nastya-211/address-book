cb02c301cbc206470be0fa62f55ba96a1f722ab0
ABurmistrova <ABurmistrova@ES-ABURMISTROVA>
2020-07-23 10:06:53 +0300

    commit 2015

---
+++ addressbook_2015/addressbook_2015/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addressbook_2015
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
